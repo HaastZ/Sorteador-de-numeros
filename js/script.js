1ec7f048fa7eae76188378b532c3f8d53095bcb8
Vinicius Almeida <vinicius.vinibola@gmail.com>
2024-10-29 00:14:46 -0300

    impedindo que o usuário não envie valor

--- js/script.js
+++ js/script.js
@@ -21,12 +21,18 @@ form.addEventListener("submit", (event) => {
     for(let i = 0; i < numbers.length - 1; i++) {
         li.textContent = numbers[i]
     }
+    
+    if(quantity.value === "" || interval1 === "" || interval2 === "") {
+        alert("Erro! Não foi possível sortear.")
+    }
+    else {
+        results.append(li)
+        divResultSorteio.classList.remove("none")
+        divSortear.classList.add("none")
+        countSorteio++
+        quantitySorteio.textContent = `${countSorteio}° resultado`
+    }
 
-    results.append(li)
-    divResultSorteio.classList.remove("none")
-    divSortear.classList.add("none")
-    countSorteio++
-    quantitySorteio.textContent = `${countSorteio}° resultado`
 })
 
 buttonSortearNovamente.addEventListener("click", () => {
